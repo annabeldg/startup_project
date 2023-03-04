449e224069d55268c898a51f491005de2d3aecef
dany herscovitch <dany.herscovitch@gmail.com>
2023-03-04 12:31:58 +0000

    Added some useful lists (industry, technology)

---
+++ useful/variables.py
@@ -0,0 +1,12 @@
+industries = ['Advertising', 'Agriculture and Farming', 'Life Sciences', 'Clothing and Apparel',
+              'Commerce and Shopping', 'Community and Lifestyle', 'Consumer Electronics', 'Consumer Goods',
+              'Content and Publishing', 'Design', 'Education', 'Energy', 'Environment and Sustainability', 'Events',
+              'Financial Services', 'Food and Beverage', 'Gaming', 'Government and Military', 'Computer Hardware',
+              'Health Care', 'HumanResources', 'Software', 'Legal', 'Logistics', 'Manufacturing', 'Data and Analytics',
+              'Media and Entertainment', 'Messaging and Telecommunications', 'Music and Audio', 'Natural Resources',
+              'Navigation and Mapping', 'Payments', 'Privacy and Security', 'Professional Services',
+              'Real Estate and Construction', 'Sales and Marketing', 'Sports', 'Transportation', 'Travel and Tourism',
+              'Video']
+
+technologies = ['Artificial Intelligence', 'Biotechnology', 'Hardware', 'Science and Engineering', 'Software',
+                'Sustainability', 'BlockChain', 'AR and VR']
