70095a1ddceb012a0f82c4a8ad20b75cd1107c3a
baptiste boninsegna <baptiste.boninsegna@hotmail.com>
2023-03-04 12:22:40 +0000

    preprocessing - date format

---
+++ start_up.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+import numpy as np
+
+from preprocessing.format_dates import format_dates
+
+data_path=os.path.join(os.path.abspath(os.getcwd()),'raw_data')
+df=pd.read_csv(os.path.join(data_path,'data_startup_final.csv'))
+df.drop_duplicates(inplace=True)
+
+date_columns=['founded_on', 'went_public_on', 'exited_on','last_funding_at','announcedOn']
+
+df = format_dates(df, date_columns)
+print(df['exited_on'].dtype, df['name'].dtype)
