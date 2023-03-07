b663afe1fe416e6cfded16899406c7e3e88b6606
dany herscovitch <dany.herscovitch@gmail.com>
2023-03-07 21:11:47 +0000

    changes but not done

--- preprocessing/format_last_equity_funding_total_moneyRaised.py
+++ preprocessing/format_last_equity_funding_total_moneyRaised.py
@@ -7,11 +7,14 @@ def format_last_equity_funding_total_moneyRaised(df):
     '''
 
     def preprocess_moneyRaised(x):
-        return float(json.loads(x)["amountUSD"])
+        try:
+            return float(json.loads(x)["amountUSD"])
+        except:
+            pass
 
     for column in ['last_equity_funding_total', 'moneyRaised']:
-        df[column].replace(np.nan,'{"amount": 0, "currency": "USD", "amountUSD": 0}', inplace=True)
-        df[column].replace('{"value":null,"currency":"USD","value_usd":null}','{"amount": 0, "currency": "USD", "amountUSD": 0}', inplace=True)
+        #df[column].replace(np.nan,'{"amount": 0, "currency": "USD", "amountUSD": 0}', inplace=True)
+        #df[column].replace('{"value":null,"currency":"USD","value_usd":null}','{"amount": 0, "currency": "USD", "amountUSD": 0}', inplace=True)
         df[column] = df[column].apply(lambda x: preprocess_moneyRaised(x))
 
     return df
