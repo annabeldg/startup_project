5d667cab48488cc42d7b5b2d42d8d376ccab483d
baptiste boninsegna <baptiste.boninsegna@hotmail.com>
2023-03-07 20:33:59 +0000

    cleaning

--- start_up.py
+++ start_up.py
@@ -7,12 +7,14 @@ from preprocessing.format_last_equity_funding_total_moneyRaised import format_la
 from preprocessing.format_object_float_int_columns import format_object_float_int_columns
 
 data_path=os.path.join(os.path.abspath(os.getcwd()),'raw_data')
-df=pd.read_csv(os.path.join(data_path,'data_startup_final.csv'))
+df=pd.read_csv(os.path.join(data_path,'startups.csv'))
 df.drop_duplicates(inplace=True)
+df.drop(columns=['website', 'short_description'],inplace = True)
 
 df= format_last_equity_funding_total_moneyRaised(df)
-df = format_dates_columns(df) #missing values in columns: went_public_on, exited_on, announcedOn
-df = create_target(df) #missing values in columns: time_to_success
-df=format_object_float_int_columns(df) #to check, notably last_equity_funding_type and stage
+df = format_dates_columns(df)
+df = create_target(df)
+#df=format_object_float_int_columns(df)
 
+print(df.isnull().sum())
 print(df.shape)
