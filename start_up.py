6fbc19dbea3e66430869560900044e3fc71d1c14
dany herscovitch <dany.herscovitch@gmail.com>
2023-03-09 19:40:50 +0000

    Ready to start working together

--- start_up.py
+++ start_up.py
@@ -12,19 +12,14 @@ data_path = os.path.join(os.path.abspath(os.getcwd()),'raw_data')
 df = pd.read_csv(os.path.join(data_path,'startups.csv'))
 print('Dataset successfully read!')
 
-## DROP PROPER DUPLICATES ##
+## 1. DROP PROPER DUPLICATES ##
 
 df.drop_duplicates(inplace=True)
 
-## DROP LEFT-JOINED DUPLICATES (+ ENCODING OF INDUSTRY, TECHNOLOGY, ROUNDS) ##
+## 2. DROP LEFT-JOINED DUPLICATES (+ ENCODING OF INDUSTRY, TECHNOLOGY, ROUNDS) ##
 
 # Convert moneyRaised columns from json to float
 df['moneyRaised'] = df['moneyRaised'].map(lambda x: json_to_float(x), na_action='ignore')
-
-# Format dates columns to datetime
-# df = format_dates_columns(df) #missing values in columns: went_public_on, exited_on, announcedOn
-
-## ENCODING ##
 
 # Encode industry, technology, and funding_rounds columns as a separate dataframe
 data_ind = industry_encoding(df)
@@ -32,13 +27,41 @@ data_tec = technology_encoding(df)
 data_fun = funding_encoding(df)
 
 # Merge all in the main df
-data_encoded = merged_all(df, data_ind, data_tec, data_fun)
+data_nodup = merged_all(df, data_ind, data_tec, data_fun)
 
+## 3. ENCODING & FEATURE ENGINEERING THE REST ##
+
+# Drop useless columns
+data_nodup.drop(columns=['id', 'name', 'website', 'short_description', 'ipo_status', 'headquartersRegion'],
+                inplace=True)
+
+to_encode = ['last_equity_funding_type', 'last_equity_funding_total', 'headquartersCountry']
+to_engineer = ['founded_on', 'last_funding_at']
 
 # Encode the target variable
-data_encoded = create_target(data_encoded) #missing values in columns: time_to_success
+data_encoded = create_target(data_nodup) #missing values in columns: time_to_success
 
-# Encode last_equity_funding_type!!
+## 4. OUTLIERS ##
+
+prone_to_outliers = ['last_equity_funding_total', 'Round 1 --> 5']
+# Do not forget to /100 last_equity_funding_total
+
+## 5. MISSING DATA ##
+
+columns_where_misses = ['last_equity_funding_type', 'last_equity_funding_total'
+                        'headquartersCountry', 'employeeCount', 'Round 1-->5']
+
+## 6. SCALING ##
+
+
 print(data_encoded.shape)
 
-# data_encoded.to_csv(os.path.join(data_path, 'startups_modified.csv'), index=False)
+
+
+data_encoded.to_csv(os.path.join(data_path, 'startups_modified.csv'), index=False)
+
+## IF WE HAVE SOME TIME ##
+
+# 1. Keep announcedOn of the first round
+# 2. NLP on short_description
+# 3.
