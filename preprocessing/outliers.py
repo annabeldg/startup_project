04675b7492fc7e28ccaed98a2f68a2b61c4bee08
dany herscovitch <dany.herscovitch@gmail.com>
2023-03-09 21:40:41 +0000

    Added outliers and missing_data files, modified start_up.py

---
+++ preprocessing/outliers.py
@@ -0,0 +1,10 @@
+## STRATEGY ##
+
+# last_equity_funding_total:
+# Remove < 20,000
+# Remove > 1,000,000,000
+
+# Finally don't touch Round 1 -> 5
+
+
+prone_to_outliers = ['last_equity_funding_total', 'Round 1 --> 5']
